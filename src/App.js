2f3da6f2cef11a2c51605e64d11ca8f4254d6d6d
Yuqi Yang <yuqiyang@Yuqi-Yangs-MacBook-Pro.local>
2019-12-18 22:27:57 -0500

    set up router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React, {Component} from 'react';
+import Navbar from './components/Navbar'
+import{ BrowserRouter, Route} from 'react-router-dom'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
 
 
 class App extends Component {
   render(){
     return(
+      <BrowserRouter>
       <div className="app">
+        <Navbar />
+        <Route exact path='/' component={Home} />
+        <Route path='/About' component={About} />
+        <Route path='/Contact' component={Contact} />
 
       </div>
+      </BrowserRouter>
     )
     }  
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,8 +4,15 @@ const Navbar =()=>{
     return(
         <nav className="nav-wrapper red darken-3">
             <div className="container">
-                <a classname="brand-logo">Poke Times</a>
+                <div className="brand-logo">Poke Times</div>
+                <ul className="right">
+                    <li><a href="/">Home</a></li>
+                    <li><a href="/about">About</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                </ul>
             </div>
         </nav>
     )
 }
+
+export default Navbar
