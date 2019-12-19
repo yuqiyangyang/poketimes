13651a9a9c188f51cf5c6ef6d2ec3c8480a57a48
Yuqi Yang <yuqiyang@Yuqi-Yangs-MacBook-Pro.local>
2019-12-18 21:51:50 -0500

    create components

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Navbar =()=>{
+    return(
+        <nav className="nav-wrapper red darken-3">
+            <div className="container">
+                <a classname="brand-logo">Poke Times</a>
+            </div>
+        </nav>
+    )
+}
