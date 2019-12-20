e1ccf9841d1d6da407325bb2c8b218428154df3b
Yuqi Yang <yuqiyang@Yuqi-Yangs-MacBook-Pro.local>
2019-12-19 23:20:22 -0500

    add image to card

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Contact from './components/Contact'
 import Post from './components/Post'
 
 
+
 class App extends Component {
   render(){
     return(
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import axios from 'axios'
 import {Link } from 'react-router-dom'
+import Pokeball from '../pokeball.png'
 
 
 
@@ -23,9 +24,10 @@ class Home extends Component {
             posts.map(post => {
                 return (
                     <div className="post card" key={post.id}>
+                        <img src={Pokeball} alt="A Pokeball"/>
                         <div className="card-content">
                             <Link to={'/' + post.id}>
-                            <span className="card-title">{post.title}</span>
+                            <span className="card-title red-text">{post.title}</span>
                             </Link >
                             <p>{post.body}</p>
 
@@ -38,7 +40,7 @@ class Home extends Component {
                 )
 
         return (
-            <div className=" container">
+            <div className=" container home">
                 <h4 className="center">Home</h4>
 
                 {postList}
